f7048c35f0c6fa9c0308beb888f614dec58560a3
Sithum Witharana <witharana92@gmail.com>
2022-12-15 11:56:30 +0530

    Add project files.

---
+++ FitnessCenterManagement/UI/frmLogin.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FitnessCenterManagement
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new MetroFramework.Controls.MetroPanel();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlMain.BackColor = System.Drawing.Color.LightGray;
+            this.pnlMain.HorizontalScrollbarBarColor = true;
+            this.pnlMain.HorizontalScrollbarHighlightOnWheel = false;
+            this.pnlMain.HorizontalScrollbarSize = 10;
+            this.pnlMain.Location = new System.Drawing.Point(-2, 68);
+            this.pnlMain.MaximumSize = new System.Drawing.Size(620, 240);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(620, 240);
+            this.pnlMain.TabIndex = 0;
+            this.pnlMain.VerticalScrollbarBarColor = true;
+            this.pnlMain.VerticalScrollbarHighlightOnWheel = false;
+            this.pnlMain.VerticalScrollbarSize = 10;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 310);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnlMain);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(620, 310);
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.Resizable = false;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroPanel pnlMain;
+    }
+}
+
